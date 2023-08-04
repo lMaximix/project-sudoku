f14676cac2fd462174b2cb35d509a73e570cbffb
Nikolai <nik7410@yandex.ru>
2023-08-04 11:16:48 +0300

    Complete matrix

--- runner.js
+++ runner.js
@@ -6,8 +6,4 @@ const readAndSolve = require('./readAndSolve');
 
 // Все судоку для решения доступны в файле puzzles.txt.
 // Прочесть файл puzzles.txt в кодировке 'utf-8' и передать его содержимое в функцию readAndSolve.
-fs.readFile(
-  './puzzles.txt',
-  'utf-8',
-  readAndSolve,
-);
+fs.readFile('./puzzles.txt', 'utf-8', readAndSolve);
--- sudoku.js
+++ sudoku.js
@@ -1,27 +1,34 @@
+const { EOL } = require('os');
 /**
  * Принимает игровое поле в формате строки — как в файле sudoku-puzzles.txt.
  * Возвращает игровое поле после попытки его решить.
  * Договорись со своей командой, в каком формате возвращать этот результат.
  */
-function solve(boardString) {
+function solve(puzzle) {
+  const arr = puzzle.split('');
+  console.log(arr);
+  function spl(arr) {
+    const res = [];
+    for (let i = 0; i < arr.length; i += 9) {
+      res.push(arr.slice(i, i + 9));
+    }
+    return res;
+  }
+  console.log(spl(arr));
 }
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает булевое значение — решено это игровое поле или нет.
  */
-function isSolved(board) {
-
-}
+function isSolved(board) {}
 
 /**
  * Принимает игровое поле в том формате, в котором его вернули из функции solve.
  * Возвращает строку с игровым полем для последующего вывода в консоль.
  * Подумай, как симпатичнее сформировать эту строку.
  */
-function prettyBoard(board) {
-
-}
+function prettyBoard(board) {}
 
 // Экспортировать функции для использования в другом файле (например, readAndSolve.js).
 module.exports = {
